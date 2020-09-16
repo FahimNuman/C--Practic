1e6160583a435cf29cdf6c8a82cb0641f08091b5
Fahim Numan <fahiomnuman87@gmail.com>
2020-09-16 20:15:05 +0600

    for loop example

---
+++ forloop example/forloop example/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace forloop_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for(int i=1;i<=15;i++)
+            {
+                Console.WriteLine("{0}*{1}={2}",num,i,num*i);
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
