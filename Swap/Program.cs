6b571e5fb36cd28bc0b799fcf44af60e83d7f550
Fahim Numan <fahiomnuman87@gmail.com>
2020-10-13 15:25:58 +0600

    Swap

---
+++ Swap/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace c_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2, temp;
+            Console.WriteLine("input first number:");
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("input Second number:");
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            temp = number1;
+            number1 = number2;
+            number2 = temp;
+
+
+            Console.WriteLine("\nFirst number" + number1);
+            Console.WriteLine("\nSecond number" + number2);
+
+        }
+    }
+}
